84e236f46a81c5017e9881c576bf8827d7b24eba
JOSH SPENCE <joshua.spence@hotmail.co.uk>
2024-04-09 18:36:29 +0100

    tried changing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from "vitest/config";
+import { fileURLToPath } from "url";
 
 export default defineConfig({
   test: {
     globals: true,
+  },
+
+  build: {
+    outDir: "dist",
+
+    rollupOptions: {
+      input: {
+        index: fileURLToPath(new URL("index.html", import.meta.url)),
+        game: fileURLToPath(new URL("game.html", import.meta.url)),
+        instructions: fileURLToPath(
+          new URL("instructions.html", import.meta.url)
+        ),
+      },
+    },
   },
   base: "./",
 });
